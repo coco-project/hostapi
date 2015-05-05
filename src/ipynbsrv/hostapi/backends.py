c1f2b39ce0ec6b1156f6e15317f7d7edf01fe4e3
Michel Käser <michel.kaeser.91@gmail.com>
2015-05-05 14:26:45 +0200

    basic DockerContainerBackend implementation

--- src/ipynbsrv/hostapi/backends.py
+++ src/ipynbsrv/hostapi/backends.py
@@ -1,42 +1,135 @@
-from ipynbsrv.contract.backends import CloneableContainerBackend, CommitableContainerBackend
+from ipynbsrv.contract.backends import (CloneableContainerBackend,
+    ContainerBackendError, SnapshotableContainerBackend)
 from docker import Client
 
-'''
-'''
-class DockerContainerBackend(CloneableContainerBackend, CommitableContainerBackend):
-    '''
-    Initialites a new Docker container backend.
 
-    :param base_url: The URL or unix path to the Docker API endpoint.
+class DockerContainerBackend(SnapshotableContainerBackend):
+    '''
+    '''
+
+    '''
+    String to identify either a container is running or stopped.
+    '''
+    STOPPED_IDENTIFIER = 'Exited'
+
+    '''
+    Initializes a new Docker container backend.
+
     :param version: The Docker API version number.
+    :param base_url: The URL or unix path to the Docker API endpoint.
     '''
     def __init__(self, version, base_url='unix://var/run/docker.sock'):
         self.client = Client(base_url=base_url, version=version)
 
-    def clone_container(self, container):
-        raise NotImplementedError
-
-    def commit_container(self, container):
-        raise NotImplementedError
-
-    def container_status(self, container):
-        raise NotImplementedError
+    def container_exists(self, container):
+        containers = self.get_containers(only_running=False)
+        for returned in containers:
+            if container['id'] == returned['Id']:
+                return True
+        return False
 
     def container_is_running(self, container):
-        raise NotImplementedError
+        container = self.get_container(container)
+        return container['Status'].startswith(DockerContainerBackend.STOPPED_IDENTIFIER)
+
+    def container_snapshot_exists(self, container, name):
+        raise NotImplemented
 
     def create_container(self, container):
-        raise NotImplementedError
+        pass
+
+    def create_container_snapshot(self, container, name):
+        if not self.container_exists(container):
+            raise ValueError("Container does not exist")
+        if not name or not isinstance(name, str):
+            raise ValueError("No snapshot name defined or not a str")
+        # TODO: check if snapshot with that name and tag already exists
+        parts = name.split(':')
+        if len(parts) != 2:
+            raise ValueError("Illegal name. Must be in the form repository:tag")
+
+        repository = parts[0]
+        tag = parts[1]
+        try:
+            return self.client.commit(
+                container=container['id'],
+                repository=repository,
+                tag=tag
+            )
+        except Exception as ex:
+            raise ContainerBackendError(ex)
 
     def delete_container(self, container, force=False):
-        raise NotImplementedError
+        if not self.container_exists(container):
+            raise ValueError("Container does not exist")
+        if self.container_is_running(container) and not force:
+            raise ValueError("Container running but delete not forced")
 
-    def restart_container(self, container):
-        self.stop(force=force)
-        self.start()
+        try:
+            return self.client.remove_container(container=container['id'], force=force)
+        except Exception as ex:
+            raise ContainerBackendError(ex)
+
+    def delete_container_snapshot(self, container, snapshot):
+        if self.container_snapshot_exists(container, snapshot):
+            raise ValueError("Container snapshot does not exist")
+        raise NotImplemented
+
+    def exec_in_container(self, container, cmd):
+        if self.container_exists(container):
+            raise ValueError("Container does not exist")
+
+        try:
+            exec_id = self.client.exec_create(container=container['id'], cmd=cmd)
+            return self.client.exec_start(exec_id=exec_id, stream=False)
+        except Exception as ex:
+            raise ContainerBackendError(ex)
+
+    def get_container(self, container):
+        if not self.container_exists(container):
+            raise ValueError("Container does not exist")
+
+        containers = self.get_containers(only_running=False)
+        for returned in containers:
+            if container['id'] == returned['Id']:
+                return returned
+
+    def get_container_logs(self, container):
+        if not self.container_exists(container):
+            raise ValueError("Container does not exist")
+
+        try:
+            logs = self.client.logs(container=container['id'], stream=False, timestamps=True)
+            return filter(lambda x: len(x) > 0,  logs.split('\n'))
+        except Exception as ex:
+            raise ContainerBackendError(ex)
+
+    def get_containers(self, only_running=False):
+        try:
+            return self.client.containers(all=not only_running)
+        except:
+            pass
+
+    def restart_container(self, container, force=False):
+        try:
+            if force:
+                return self.client.restart(container=container['id'], timeout=0)
+            else:
+                return self.client.restart(container=container['id'])
+        except Exception as ex:
+            raise ContainerBackendError(ex)
+
+    def restore_container_snapshot(self, container, snapshot):
+        raise NotImplementedError
 
     def start_container(self, container):
-        raise NotImplementedError
+        pass
 
     def stop_container(self, container, force=False):
-        raise NotImplementedError
+        try:
+            if force:
+                return self.client.stop(container=container['id'], timeout=0)
+            else:
+                return self.client.stop(container=container['id'])
+        except Exception as ex:
+            raise ContainerBackendError(ex)
--- src/ipynbsrv/hostapi/routes/containers.py
+++ src/ipynbsrv/hostapi/routes/containers.py
@@ -1,78 +1,132 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from ipynbsrv.hostapi.backends import DockerContainerBackend
+from ipynbsrv.hostapi.routes.common import error_response, success_response
 
 '''
 '''
 blueprint = Blueprint('name', __name__, url_prefix='/containers')
+container_backend = DockerContainerBackend(version="1.18")
 
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/exec', methods=['POST'])
-def exec_in_container(ct_id):
-    return "EXEC CONTAINER"
-
-'''
-'''
-@blueprint.route('/<int:ct_id>/clone', methods=['POST'])
+@blueprint.route('/<ct_id>/clone', methods=['POST'])
 def clone_container(ct_id):
+    '''
+    '''
     return "CLONE CONTAINER"
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/commit', methods=['POST'])
-def commit_container(ct_id):
-    return "COMMIT CONTAINER"
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/logs', methods=['GET'])
+@blueprint.route('/<ct_id>/exec', methods=['POST'])
+def exec_in_container(ct_id):
+    '''
+    '''
+    try:
+        return success_response(container_backend.exec_in_container(
+            {'id': ct_id},
+            request.form.get('cmd')
+        ))
+    except:
+        return error_response(503, "Something went wrong")
+
+
+@blueprint.route('/<ct_id>/logs', methods=['GET'])
 def get_container_logs(ct_id):
-    return "GET CONTAINER LOGS"
+    '''
+    '''
+    try:
+        return success_response(container_backend.get_container_logs({
+            'id': ct_id
+        }))
+    except:
+        return error_response(503, "Something went wrong")
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/public_key', methods=['GET'])
+
+@blueprint.route('/<ct_id>/public_key', methods=['GET'])
 def get_public_key(ct_id):
+    '''
+    '''
     return "PUBLIC KEY CONTAINER"
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/restart', methods=['GET'])
-def restart_container(ct_id):
-    return "STOP CONTAINER"
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/start', methods=['GET'])
+@blueprint.route('/<ct_id>/restart', methods=['GET'])
+def restart_container(ct_id):
+    '''
+    '''
+    try:
+        return success_response(container_backend.restart_container({
+            'id': ct_id
+        }))
+    except:
+        return error_response(503, "Something went wrong")
+
+
+@blueprint.route('/<ct_id>/snapshot', methods=['POST'])
+def snapshot_container(ct_id):
+    '''
+    '''
+    try:
+        return success_response(container_backend.snapshot_container(
+            {'id': ct_id},
+            request.form.get('name')
+        ))
+    except:
+        return error_response(503, "Something went wrong")
+
+
+@blueprint.route('/<ct_id>/start', methods=['POST'])
 def start_container(ct_id):
+    '''
+    '''
     return "START CONTAINER"
 
-'''
-'''
-@blueprint.route('/<int:ct_id>/stop', methods=['GET'])
+
+@blueprint.route('/<ct_id>/stop', methods=['GET'])
 def stop_container(ct_id):
-    return "STOP CONTAINER"
+    '''
+    '''
+    try:
+        return success_response(container_backend.stop_container({
+            'id': ct_id
+        }))
+    except:
+        return error_response(503, "Something went wrong")
 
-'''
-'''
-@blueprint.route('/<int:ct_id>', methods=['GET'])
+
+@blueprint.route('/<ct_id>', methods=['GET'])
 def get_container(ct_id):
-    return "GET CONTAINER"
+    '''
+    '''
+    try:
+        return success_response(container_backend.get_container({
+            'id': ct_id
+        }))
+    except:
+        return error_response(503, "Something went wrong")
 
-'''
-'''
-@blueprint.route('/<int:ct_id>', methods=['DELETE'])
+
+@blueprint.route('/<ct_id>', methods=['DELETE'])
 def delete_container(ct_id):
-    return "DELETE CONTAINER"
+    '''
+    '''
+    try:
+        return success_response(container_backend.delete_container({
+            'id': ct_id
+        }))
+    except:
+        return error_response(503, "Something went wrong")
 
-'''
-'''
+
 @blueprint.route('', methods=['POST'])
 def create_container():
+    '''
+    '''
     return "CREATE CONTAINER"
 
-'''
-'''
+
 @blueprint.route('', methods=['GET'])
 def get_containers():
-    return "GET CONTAINERS"
+    '''
+    '''
+    try:
+        return success_response(container_backend.get_containers())
+    except:
+        return error_response(503, "Something failed")
