f1f99c09f0e4ec33f1a198a38bb306d745c153aa
Michel Käser <mail@michelkaeser.ch>
2015-07-14 11:59:50 +0200

    fix pydoc style; move generic config values to top

--- src/ipynbsrv/hostapi/config.py
+++ src/ipynbsrv/hostapi/config.py
@@ -1,15 +1,14 @@
-'''
-Configuration option that will store a reference to the container backend
-implementation the API should use.
-
-This option can be set with --container-backend CONTAINER_BACKEND on start.
-'''
-container_backend = None
-
-
-'''
+"""
 Stores a Boolean indicating if the app should run in debug mode or not.
 
 This option can be set with -d or --debug on start.
-'''
+"""
 debug = False
+
+
+"""
+Variable storing a reference to the container backend implementation the API should use.
+
+This option can be set with --container-backend CONTAINER_BACKEND on start.
+"""
+container_backend = None
